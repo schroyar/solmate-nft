750d803e256da30bfdacd08bec808fe2008a51d6
schroyar <100044502+schroyar@users.noreply.github.com>
2022-03-30 23:54:16 -0300

    added receiver contract from erc721

--- src/test/Contract.t.sol
+++ src/test/Contract.t.sol
@@ -4,6 +4,17 @@ pragma solidity 0.8.10;
 import "ds-test/test.sol";
 import "../Contract.sol";
 
+contract Receiver is ERC721TokenReceiver {
+    function onERC721Received(
+        address operator,
+        address from,
+        uint256 id,
+        bytes calldata data
+    ) external returns (bytes4) {
+        return this.onERC721Received.selector;
+    }
+}
+
 contract ContractTest is DSTest {
     Contract private nft;
 
@@ -17,6 +28,11 @@ contract ContractTest is DSTest {
 
     function testMintYes() public {
         nft.mintTo{value: 0.08 ether}(address(1));
+    }
+
+    function testWithdraw() public {
+        Receiver receiver = new Receiver();
+
     }
 
 
